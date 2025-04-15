cd539971f9f4d15776ebc7fba8010621408f20ed
satabdigoswami87@gmail.com <satabdigoswami87@gmail.com>
2025-04-15 22:33:00 +0530

    Fixed image path casing for Vercel

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,36 +6,36 @@ import Footer from './Footer';
 export default function Home() {
 
   const menClothes = [
-    {num: 1, name: 'Casual Shirt', image: '/images/men.jpeg', link: '/product/casual-shirt' },
-    {num: 2,name: 'Denim Jacket', image: '/images/men.jpeg', link: '/product/denim-jacket' },
-    {num: 3,name: 'Slim Fit Jeans', image: '/images/men.jpeg', link: '/product/slim-fit-jeans' },
-    {num: 4,name: 'Graphic Tee', image: '/images/men.jpeg', link: '/product/graphic-tee' },
-    {num: 5,name: 'Cargo Pants', image: '/images/men.jpeg', link: '/product/cargo-pants' },
-    {num: 6,name: 'Leather Jacket', image: '/images/men.jpeg', link: '/product/leather-jacket' },
+    {num: 1, name: 'Casual Shirt', image: '/Images/men.jpeg', link: '/product/casual-shirt' },
+    {num: 2,name: 'Denim Jacket', image: '/Images/men.jpeg', link: '/product/denim-jacket' },
+    {num: 3,name: 'Slim Fit Jeans', image: '/Images/men.jpeg', link: '/product/slim-fit-jeans' },
+    {num: 4,name: 'Graphic Tee', image: '/Images/men.jpeg', link: '/product/graphic-tee' },
+    {num: 5,name: 'Cargo Pants', image: '/Images/men.jpeg', link: '/product/cargo-pants' },
+    {num: 6,name: 'Leather Jacket', image: '/Images/men.jpeg', link: '/product/leather-jacket' },
   ];
   const womenClothes = [
-    {num: 1, name: 'Casual Shirt', image: '/images/women.jpeg', link: '/product/casual-shirt' },
-    {num: 2,name: 'Denim Jacket', image: '/images/women.jpeg', link: '/product/denim-jacket' },
-    {num: 3,name: 'Slim Fit Jeans', image: '/images/women.jpeg', link: '/product/slim-fit-jeans' },
-    {num: 4,name: 'Graphic Tee', image: '/images/women.jpeg', link: '/product/graphic-tee' },
-    {num: 5,name: 'Cargo Pants', image: '/images/women.jpeg', link: '/product/cargo-pants' },
-    {num: 6,name: 'Leather Jacket', image: '/images/women.jpeg', link: '/product/leather-jacket' },
+    {num: 1, name: 'Casual Shirt', image: '/Images/women.jpeg', link: '/product/casual-shirt' },
+    {num: 2,name: 'Denim Jacket', image: '/Images/women.jpeg', link: '/product/denim-jacket' },
+    {num: 3,name: 'Slim Fit Jeans', image: '/Images/women.jpeg', link: '/product/slim-fit-jeans' },
+    {num: 4,name: 'Graphic Tee', image: '/Images/women.jpeg', link: '/product/graphic-tee' },
+    {num: 5,name: 'Cargo Pants', image: '/Images/women.jpeg', link: '/product/cargo-pants' },
+    {num: 6,name: 'Leather Jacket', image: '/Images/women.jpeg', link: '/product/leather-jacket' },
   ];
   const kidsClothes = [
-    {num: 1, name: 'Casual Shirt', image: '/images/kids.jpeg', link: '/product/casual-shirt' },
-    {num: 2,name: 'Denim Jacket', image: '/images/kids.jpeg', link: '/product/denim-jacket' },
-    {num: 3,name: 'Slim Fit Jeans', image: '/images/kids.jpeg', link: '/product/slim-fit-jeans' },
-    {num: 4,name: 'Graphic Tee', image: '/images/kids.jpeg', link: '/product/graphic-tee' },
-    {num: 5,name: 'Cargo Pants', image: '/images/kids.jpeg', link: '/product/cargo-pants' },
-    {num: 6,name: 'Leather Jacket', image: '/images/kids.jpeg', link: '/product/leather-jacket' },
+    {num: 1, name: 'Casual Shirt', image: '/Images/kids.jpeg', link: '/product/casual-shirt' },
+    {num: 2,name: 'Denim Jacket', image: '/Images/kids.jpeg', link: '/product/denim-jacket' },
+    {num: 3,name: 'Slim Fit Jeans', image: '/Images/kids.jpeg', link: '/product/slim-fit-jeans' },
+    {num: 4,name: 'Graphic Tee', image: '/Images/kids.jpeg', link: '/product/graphic-tee' },
+    {num: 5,name: 'Cargo Pants', image: '/Images/kids.jpeg', link: '/product/cargo-pants' },
+    {num: 6,name: 'Leather Jacket', image: '/Images/kids.jpeg', link: '/product/leather-jacket' },
   ];
   const petsClothes = [
-    {num: 1, name: 'Casual Shirt', image: '/images/pets.jpeg', link: '/product/casual-shirt' },
-    {num: 2,name: 'Denim Jacket', image: '/images/pets.jpeg', link: '/product/denim-jacket' },
-    {num: 3,name: 'Slim Fit Jeans', image: '/images/pets.jpeg', link: '/product/slim-fit-jeans' },
-    {num: 4,name: 'Graphic Tee', image: '/images/pets.jpeg', link: '/product/graphic-tee' },
-    {num: 5,name: 'Cargo Pants', image: '/images/pets.jpeg', link: '/product/cargo-pants' },
-    {num: 6,name: 'Leather Jacket', image: '/images/pets.jpeg', link: '/product/leather-jacket' },
+    {num: 1, name: 'Casual Shirt', image: '/Images/pets.jpeg', link: '/product/casual-shirt' },
+    {num: 2,name: 'Denim Jacket', image: '/Images/pets.jpeg', link: '/product/denim-jacket' },
+    {num: 3,name: 'Slim Fit Jeans', image: '/Images/pets.jpeg', link: '/product/slim-fit-jeans' },
+    {num: 4,name: 'Graphic Tee', image: '/Images/pets.jpeg', link: '/product/graphic-tee' },
+    {num: 5,name: 'Cargo Pants', image: '/Images/pets.jpeg', link: '/product/cargo-pants' },
+    {num: 6,name: 'Leather Jacket', image: '/Images/pets.jpeg', link: '/product/leather-jacket' },
   ];
 
   return (
@@ -45,7 +45,7 @@ export default function Home() {
             <h2 className="section-title">Shop by Category</h2>
             <div className="category-cards">
                 <div className="card">
-                     <img src="/images/men.jpeg" alt="Men's Fashion" />
+                     <img src="/Images/men.jpeg" alt="Men's Fashion" />
                      <div className="card-content">
                        <h3>Men</h3>
                        <Link to="/Explore/men">Explore <i className='fas fa-angle-right'></i></Link>
@@ -53,7 +53,7 @@ export default function Home() {
                 </div>
 
                 <div className="card" style={{animationDelay:'0.7s'}}>
-                  <img src="/images/women.jpeg" alt="Women's Fashion" />
+                  <img src="/Images/women.jpeg" alt="Women's Fashion" />
                   <div className="card-content">
                     <h3>Women</h3>
                     <Link to="/Explore/women">Explore <i className='fas fa-angle-right'></i></Link>
@@ -61,7 +61,7 @@ export default function Home() {
                 </div>
 
                 <div className="card" style={{animationDelay:'1.4s'}}>
-                  <img src="/images/kids.jpeg" alt="Kids' Fashion" />
+                  <img src="/Images/kids.jpeg" alt="Kids' Fashion" />
                   <div className="card-content">
                     <h3>Children</h3>
                     <Link to="/Explore/kids">Explore  <i className='fas fa-angle-right'></i></Link>
@@ -69,7 +69,7 @@ export default function Home() {
                 </div>
 
                 <div className="card" style={{animationDelay:'2.1s'}}>
-                  <img src="/images/pets.jpeg" alt="pets' Fashion" />
+                  <img src="/Images/pets.jpeg" alt="pets' Fashion" />
                   <div className="card-content">
                     <h3>Pets</h3>
                     <Link to="/Explore/pets">Explore  <i className='fas fa-angle-right'></i></Link>
